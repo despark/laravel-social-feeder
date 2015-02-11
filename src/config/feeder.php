b2cd5433796341ec6b83b2c7ea9469046cfcccd1
Yavor Mihaylov <yavor.st.m@gmail.com>
2015-02-11 18:27:13 +0200

    Set placeholders in config file

--- src/config/feeder.php
+++ src/config/feeder.php
@@ -2,13 +2,13 @@
 
 return array(
 	'twitterCredentials' => array(
-		'consumerKey' => 'JvXHFRFtON6PFeqzzPKuYzqWl',
-		'consumerSecret' => 'C4hjh8k9TTUVqXTFjb4GdnK9IeYc4YGdpa6Hl8Uvc4G4nFuEaX',
-		'accessToken' => '192664878-cho3bZ0piKCJaCqQTCHl0eyFOJZo3Lj363GPmzhx',
-		'accessTokenSecret' => 'dhvsEOMGGmo3txrLr7lXse6rxmUe5fcM3xTxCW18jRCCN',
+		'consumerKey' => '{consumer_key}',
+		'consumerSecret' => '{consumer_secret}',
+		'accessToken' => '{access_token}',
+		'accessTokenSecret' => '{access_token_secret}',
 
 		'host' => 'https://api.twitter.com/1.1/',
 
-        'screen_name' => 'paperhatgroup',
+        'screen_name' => '{screen_name}',
 	),
 );
